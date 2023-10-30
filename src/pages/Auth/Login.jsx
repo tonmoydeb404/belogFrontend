21c40ce4f29acc1cae6b79da308689dfd314f1d9
tonmoydeb404 <tonmoydeb404@gmail.com>
2023-10-31 05:07:46 +0600

    updated login page

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,9 +1,37 @@
-import LoginForm from "../../components/forms/auth/LoginForm";
+import { Button } from "react-daisyui";
+import { BiHome, BiLogoGithub, BiLogoGoogle } from "react-icons/bi";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <div className="app_container min-h-screen flex flex-col items-center justify-center">
-      <LoginForm />
+      <div className="p-5 rounded bg-neutral w-full max-w-lg shadow-xl">
+        <div className="flex justify-between mb-5">
+          <div>
+            <h2 className="text-2xl font-bold mb-1">Sign In</h2>
+            <p className="text-base opacity-70">to continue you need to...</p>
+          </div>
+
+          <Link className="text-2xl text-primary" to={"/"}>
+            <BiHome />
+          </Link>
+        </div>
+
+        <div className="flex flex-col gap-1.5">
+          <Button
+            className="text-white bg-[#4183F3] hover:bg-[#3f6cb8]"
+            endIcon={<BiLogoGoogle className="text-xl" />}
+          >
+            Sign in with Google
+          </Button>
+          <Button
+            className="text-white bg-black"
+            endIcon={<BiLogoGithub className="text-xl" />}
+          >
+            Sign in with Github
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
